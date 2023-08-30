22adbee80e9bb0581d3b5e83647a2e8979de1975
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-08-30 09:59:28 +0500

    TODO

---
+++ src/Components/Tode.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import "./Todo.css"
+
+function Tode() {
+    const [list,setList] = useState([
+        {id: 1, task: "Play", done: false}
+    ])
+    const [task,setTask] = useState('')
+  return (
+    <div className='container'> 
+        <div className='inputPart'>
+            <h2>My Todo APP</h2>
+            <input type="text"  placeholder='Add Task' onChange={e => setTask(e.target.value)} />
+            <button className='addBtn' onClick={Add}>Add</button>
+        </div>
+        <div className='taskList'>
+            <ul>
+               {list.map((l,i) => (
+                <li >  
+                 <span onClick={e =>Uptade(l.id )} className={l.done ? "done" : ""}>{l.task}</span> 
+                <span onClick={e => Remove(l.id)}>x</span></li>
+               ))}
+            </ul>
+        </div>
+    </div>
+  )
+
+  function Add() {
+    const newObj = {id: 2, task: task,done: false }
+    setList(prevList => prevList.concat(newObj))
+  }
+
+  function Uptade(id){
+const newList = list.map((l,i) =>(
+    l.id === id ? {...l, done: true  } : l
+))
+
+setList(newList)
+  }
+
+  function Remove (id) {
+    const newList = list.filter (f => f.id !== id );
+    setList(newList)
+  }
+
+}
+
+export default Tode
